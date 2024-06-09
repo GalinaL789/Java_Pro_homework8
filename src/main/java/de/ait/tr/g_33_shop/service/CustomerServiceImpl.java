4b2a24c0f99785d7d63a203f98f3a50f09ee534f
Galina Lasko <galina_lasko@hotmail.com>
2024-06-09 20:31:02 +0200

    changes in CustomerServiceImpl

--- src/main/java/de/ait/tr/g_33_shop/service/CustomerServiceImpl.java
+++ src/main/java/de/ait/tr/g_33_shop/service/CustomerServiceImpl.java
@@ -1,19 +1,18 @@
 package de.ait.tr.g_33_shop.service;
 
 import de.ait.tr.g_33_shop.domain.entity.Customer;
+import de.ait.tr.g_33_shop.domain.entity.Product;
 import de.ait.tr.g_33_shop.repository.CustomerRepository;
 import de.ait.tr.g_33_shop.service.interfaces.CustomerService;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
-public class CustomerServiceImpl implements CustomerService
-{
-    private CustomerRepository customerRepository;
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -21,19 +20,27 @@ public class CustomerServiceImpl implements CustomerService
 
     @Override
     public Customer save(Customer customer) {
-        return   customerRepository.save(customer);
+        return customerRepository.save(customer);
     }
+
+//    @Override
+//    public List<Customer> getAllActiveCustomers() {
+//        List<Customer> activeCustomers = new ArrayList<>();
+//        List<Customer> allCustomers = customerRepository.findAll();
+//        for (Customer customer : allCustomers) {
+//            if (customer.isActive()) {
+//                activeCustomers.add(customer);
+//            }
+//        }
+//        return activeCustomers;
+//    }
 
     @Override
     public List<Customer> getAllActiveCustomers() {
-        List<Customer> activeCustomers = new ArrayList<>();
-        List<Customer> allCustomers = customerRepository.findAll();
-       for (Customer customer : allCustomers) {
-           if (customer.isActive()) {
-               activeCustomers.add(customer);
-           }
-       }
-        return  activeCustomers;
+        return customerRepository.findAll()
+                .stream()
+                .filter(Customer::isActive)
+                .toList();
     }
 
 //    @Override
@@ -41,10 +48,18 @@ public class CustomerServiceImpl implements CustomerService
 //        return customerRepository.getOne(id);
 //    }
 
-    @Override
+        @Override
     public Customer getById(Long id) {
         return customerRepository.findById(id).orElse(null);
     }
+//    @Override
+//    public Customer getById(Long id) {
+//        Customer customer = customerRepository.findById(id).orElse(null);
+//        if (customer != null && customer.isActive()) {
+//            return customer;
+//        }
+//        return null;
+//    }
 
     @Override
     public Customer update(Customer customer) {
@@ -58,7 +73,7 @@ public class CustomerServiceImpl implements CustomerService
 
     @Override
     public void restoreById(Long id) {
-        System.out.println("Restore Customer: "+id);
+        System.out.println("Restore Customer: " + id);
     }
 
     @Override
